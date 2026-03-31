669e26ad0351c48796a4564ff4a02718284da714
ZORO0910 <rraj51045@gmail.com>
2026-03-31 19:18:58 +0530

    Add files via upload

---
+++ P1/HTML UI.cpp
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Smart Campus System</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1>Complaint System</h1>
+
+<input id="regNo" placeholder="Registration No">
+<input id="name" placeholder="Name">
+<input id="room" placeholder="Room No">
+<input id="issue" placeholder="Issue">
+
+<button onclick="addComplaint()">Add Complaint</button>
+
+<h2>Complaints</h2>
+<div id="list"></div>
+
+<script src="script.js"></script>
+</body>
+</html>
